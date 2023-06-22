0658d424088d12f0d34d5d47e5baca4663fdd008
Albert <albert.minnibaeff@yandex.ru>
2023-06-22 23:06:39 +0400

    homework-1

--- src/channel.py
+++ src/channel.py
@@ -14,11 +14,15 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Channel:
     """Класс для ютуб-канала"""
 
+
     def __init__(self, channel_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Экземпляр инициализируется id канала.
+        Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.channel_id,
+        part='snippet,statistics').execute()
         return print(channel)
