153c68143aeb67913ad78b3555a1bbe10ba590b0
Albert <albert.minnibaeff@yandex.ru>
2023-06-29 01:11:37 +0400

    homework-3

--- src/channel.py
+++ src/channel.py
@@ -30,6 +30,34 @@ class Channel:
         self.view_count: int = self.channel["items"][0]["statistics"]["viewCount"]
 
 
+    def __str__(self):
+        return f"'{self.title} {self.url}'"
+
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.__channel_id,
