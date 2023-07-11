1cd2a4430918526ca9f4a13103a496d5c2cc8b46
Albert <albert.minnibaeff@yandex.ru>
2023-07-11 20:14:51 +0400

    homework-6

--- src/video.py
+++ src/video.py
@@ -15,16 +15,24 @@ class Video:
 
     def __init__(self, video_id: str):
         self.video_id = video_id
-        self.__video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        self.title = None
+        self.url = None
+        self.view_count = None
+        self.like_count = None
+        try:
+            self.__video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=self.video_id
                                                ).execute()
-        self.video_title: str = self.__video_response['items'][0]['snippet']['title']
-        self.url: str = f"https://youtu.be/{self.video_id}"
-        self.view_count: int = self.__video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.__video_response['items'][0]['statistics']['likeCount']
+            self.title: str = self.__video_response['items'][0]['snippet']['title']
+        except IndexError:
+            print("Ошибка инициализации, возможно был передан несуществующий id видео")
+        else:
+            self.url: str = f"https://youtu.be/{self.video_id}"
+            self.view_count: int = self.__video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.__video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return f"{self.video_title}"
+        return f"{self.title}"
 
 
 class PLVideo(Video):
